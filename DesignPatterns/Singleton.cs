3286508eddc8fe963eaf15f109d8428f7a097324
Ravsaheb Shelake <Ravsaheb@zirafftechnologies.com>
2023-06-28 00:38:30 +0530

    added singleton class

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns;
+
+/// <summary>
+/// Sealed is used to avoid inheritance of the sigleton class through 
+/// which another instance can be created within the same class
+/// </summary>
+public sealed class Singleton
+{
+    private static Singleton instance;
+    private static object obj;
+
+    /// <summary>
+    /// Its a private constructor which restricts access to only within this class
+    /// </summary>
+    private Singleton() { }
+
+    /// <summary>
+    /// Public method to get the instance of singleton class
+    /// </summary>
+    /// <returns></returns>
+    public static Singleton GetInstance()
+    {
+        lock (obj)
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+        }
+
+        return instance;
+    }
+}
